a3016bdd4580f3291b41ce28e6a3b379774a49a9
Raine <akita.neru@gmx.de>
2020-04-03 16:08:40 +0200

    Overworld Test Scene
    Added Unity Terrain with randomness + Day/Night Circle

---
+++ Dextery/Assets/ActualProject/Scripts/Overworld/DayNightCycle/LightingManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    [SerializeField]
+    private Light m_directionalLight;
+    [SerializeField]
+    private LightingPreset m_preset;
+
+    [SerializeField, Range(0, 24)]
+    private float m_timeOfDay;
+
+    private void Update()
+    {
+        if(m_preset == null)
+        {
+            return;
+        }
+
+        if(Application.isPlaying)
+        {
+            m_timeOfDay += Time.deltaTime;
+            m_timeOfDay %= 24;
+            UpdateLighting(m_timeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(m_timeOfDay / 24f);
+        }
+    }
+
+    private void UpdateLighting(float _timePercent)
+    {
+        RenderSettings.ambientLight = m_preset.AmbientColor.Evaluate(_timePercent);
+        RenderSettings.fogColor = m_preset.FogColor.Evaluate(_timePercent);
+
+        if(m_directionalLight != null)
+        {
+            m_directionalLight.color = m_preset.DirectionalColor.Evaluate(_timePercent);
+            m_directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((_timePercent * 360f) - 90f, 170f, 0));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if(m_directionalLight != null)
+        {
+            return;
+        }
+
+        if(RenderSettings.sun != null)
+        {
+            m_directionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach(Light light in lights)
+            {
+                if(light.type == LightType.Directional)
+                {
+                    m_directionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
