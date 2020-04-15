3e24a86a746c510908ea9fcc43947da19eff9f0b
Mosel <mario.luetzenkirchen@online.de>
2020-04-15 15:13:10 +0200

    Basic Shop System

--- Dextery/Assets/ActualProject/Scripts/Player/Inventory/Item.cs
+++ Dextery/Assets/ActualProject/Scripts/Player/Inventory/Item.cs
@@ -8,6 +8,8 @@ public class Item
 {
     public EItems ItemType;
     public int Amount;
+    public int Value;
+    public float EffectVal;
 
     public bool IsStackable()
     {
@@ -20,5 +22,18 @@ public class Item
             default:
                 return false;
         }
+    }
+
+    public static Item CreateItem(EItems _item, int _amount = 1)
+    {
+        switch (_item)
+        {
+            case EItems.HEALPOTION:
+                return new Item { ItemType = _item, Amount = _amount, Value = 10, EffectVal = 10.0f };
+            case EItems.MANAPOTION:
+                return new Item { ItemType = _item, Amount = _amount, Value = 10, EffectVal = 10.0f };
+        }
+
+        return null;
     }
 }
