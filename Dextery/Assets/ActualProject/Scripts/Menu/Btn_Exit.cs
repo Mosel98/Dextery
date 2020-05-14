00687ca7f77feefd6a1761e38c867f8c2716efb1
Raine <akita.neru@gmx.de>
2020-05-14 18:41:03 +0200

    Main Menu animated

---
+++ Dextery/Assets/ActualProject/Scripts/Menu/Btn_Exit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class Btn_Exit : MonoBehaviour, IPointerEnterHandler, ISelectHandler, IPointerExitHandler, IDeselectHandler
+{
+    [SerializeField]
+    private Animator m_toggle;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_toggle.SetBool("isExitHighlighted", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m_toggle.SetBool("isExitHighlighted", false);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        m_toggle.SetBool("isExitHighlighted", true);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        m_toggle.SetBool("isExitHighlighted", false);
+    }
+}
---
+++ Dextery/Assets/ActualProject/Scripts/Menu/Btn_LoadGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class Btn_LoadGame : MonoBehaviour, IPointerEnterHandler, ISelectHandler, IPointerExitHandler, IDeselectHandler
+{
+    [SerializeField]
+    private Animator m_toggle;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_toggle.SetBool("isLoadHighlighted", true);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        m_toggle.SetBool("isLoadHighlighted", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m_toggle.SetBool("isLoadHighlighted", false);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        m_toggle.SetBool("isLoadHighlighted", false);
+    }
+}
